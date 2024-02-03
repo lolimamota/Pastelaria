2d8f1cdc23d536caa8ca1494b77869d61c939d0b
Lorrayne <lorraynelimamota@gmail.com>
2024-02-03 14:48:06 -0300

    styled-components add

--- pastelaria-ling-ling/src/Components/Header/Header.jsx
+++ pastelaria-ling-ling/src/Components/Header/Header.jsx
@@ -1,25 +1,44 @@
 //import tools
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 // import images
 import Logo from "./img/pastelaria.png";
 
 //funçoes do styled-components
 
+const Ul = styled.ul
+    `border: solid red;
+    list-style: none;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    `;
 
+const Img = styled.img
+    `width: 200px;
+    `;
+
+const Nav = styled.nav
+    `width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-color: black;
+    color: white;
+    `;
 
 //componente funcional
 export default function Header(){
     return(
-        <nav>
-            <img src={Logo} alt="Logo da pastelaria ling ling com dois pasteis"/>
-            <ul>
+        <Nav>
+            <Img src={Logo} alt="Logo da pastelaria ling ling com dois pasteis"/>
+            <Ul>
                 <li>Inicio</li>
                 <li>Cardapio</li>
                 <li>Delivery</li>
                 <li>Carrinho</li>
-            </ul>
-        </nav>
+            </Ul>
+        </Nav>
     )
 }

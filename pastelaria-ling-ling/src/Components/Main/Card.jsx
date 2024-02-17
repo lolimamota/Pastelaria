555d211a47f109a4b09e2729fb9f314ff6a9893d
Lorrayne <lorraynelimamota@gmail.com>
2024-02-17 15:44:57 -0300

    no modified

--- pastelaria-ling-ling/src/Components/Main/Card.jsx
+++ pastelaria-ling-ling/src/Components/Main/Card.jsx
@@ -5,7 +5,7 @@ import Carrinho from "../Main/img/carrinho.png"
 import Pastel4 from "../Main/img/cestinha_4pasteis.png"
 
 
-export default function Card({title, image, alt}) {
+export default function Card({title, image, alt, value}) {
     const [item, setItem] = useState(0);
     const Adc = () => { setItem(prevItem => prevItem <= 9 ? prevItem + 1 : prevItem) };
     const Rem = () => { setItem(prevItem => prevItem > 0 ? prevItem - 1 : prevItem) };
@@ -18,6 +18,7 @@ export default function Card({title, image, alt}) {
             </div>
             <div>
             <h2>{item}</h2>
+            <h3>{value}</h3>
             <button onClick={Rem}>Leve <img src={Menos} alt="Sinal de subtração" /></button>
             <button onClick={Adc}>Leve <img src={Mais} alt="Sinal de adição" /></button>
             <button>Comprar <img src={Carrinho} alt="Imagem de um carrinho de compra" /></button>
